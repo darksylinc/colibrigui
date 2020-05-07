darksylinc/colibrigui

commit 44200cbafa000a5402ce33e5b267210fefbb0818

Added default skin colours

src/ColibriGui/ColibriRenderable.cpp
@@ -29,6 +29,7 @@ namespace Colibri
 		ColibriOgreRenderable( Ogre::Id::generateNewId<Ogre::ColibriOgreRenderable>(),
 							   manager->getOgreObjectMemoryManager(),
 							   manager->getOgreSceneManager(), 0u, manager ),
+		m_overrideSkinColour( false ),
 		m_colour( Ogre::ColourValue::White ),
 		m_numVertices( 6u * 9u ),
 		m_currVertexBufferOffset( 0 ),
@@ -58,6 +59,19 @@ namespace Colibri
 		return m_visualsEnabled;
 	}
 	//-------------------------------------------------------------------------
+	void Renderable::setColour( bool overrideSkinColour, const Ogre::ColourValue &colour )
+	{
+		m_overrideSkinColour = overrideSkinColour;
+		if( overrideSkinColour )
+			m_colour = colour;
+		else
+			m_colour = m_stateInformation[m_currentState].defaultColour;
+	}
+	//-------------------------------------------------------------------------
+	const Ogre::ColourValue &Renderable::getColour() const { return m_colour; }
+	//-------------------------------------------------------------------------
+	bool Renderable::getOverrideSkinColour() const { return m_overrideSkinColour; }
+	//-------------------------------------------------------------------------
 	void Renderable::setSkin( Ogre::IdString skinName, States::States forState )
 	{
 		SkinManager *skinManager = m_manager->getSkinManager();
@@ -80,6 +94,9 @@ namespace Colibri
 			}
 		}
 
+		if( !m_overrideSkinColour )
+			m_colour = m_stateInformation[m_currentState].defaultColour;
+
 		setClipBordersMatchSkin();
 	}
 	//-------------------------------------------------------------------------
@@ -100,6 +117,9 @@ namespace Colibri
 				}
 			}
 		}
+
+		if( !m_overrideSkinColour )
+			m_colour = m_stateInformation[m_currentState].defaultColour;
 
 		setClipBordersMatchSkin();
 	}
@@ -138,12 +158,18 @@ namespace Colibri
 			}
 		}
 
+		if( !m_overrideSkinColour )
+			m_colour = m_stateInformation[m_currentState].defaultColour;
+
 		setClipBordersMatchSkin();
 	}
 	//-------------------------------------------------------------------------
 	void Renderable::setState( States::States state, bool smartHighlight, bool broadcastEnable )
 	{
 		Widget::setState( state, smartHighlight, broadcastEnable );
+
+		if( !m_overrideSkinColour )
+			m_colour = m_stateInformation[m_currentState].defaultColour;
 
 		if( mHlmsDatablock->getName() != m_stateInformation[m_currentState].materialName )
 			setDatablock( m_stateInformation[m_currentState].materialName );

include/ColibriGui/ColibriRenderable.h
@@ -25,6 +25,7 @@ namespace Colibri
 		float			borderSize[Borders::NumBorders];
 		float			borderRepeatSize[Borders::NumBorders]; /// 0 or negative means disable repeat
 		float			centerAspectRatio;
+		Ogre::ColourValue defaultColour;
 		Ogre::IdString	materialName;
 	};
 
@@ -96,8 +97,8 @@ namespace Colibri
 	protected:
 		StateInformation m_stateInformation[States::NumStates];
 
-	public:
-		Ogre::ColourValue	m_colour;
+		bool              m_overrideSkinColour;
+		Ogre::ColourValue m_colour;
 
 	protected:
 		/// WARNING: Most of the code assumes m_numVertices is hardcoded to 6*9;
@@ -143,6 +144,22 @@ namespace Colibri
 		*/
 		void setVisualsEnabled( bool bEnabled );
 		virtual bool isVisualsEnabled() const colibri_final;
+
+		/** Sets a custom colour
+		@param overrideSkinColour
+			When false, we ignore 'colour' argument and reset back to using the skin's default
+
+			When true, we always use the set 'colour', regardless of current state.
+			Each state may have its own skin colour, hence this overrides all of them
+		@param colour
+			The colour to use.
+			Ignored if overrideSkinColour = false
+		*/
+		void setColour( bool overrideSkinColour, const Ogre::ColourValue &colour );
+
+		const Ogre::ColourValue &getColour() const;
+
+		bool getOverrideSkinColour() const;
 
 		/** Sets an individual skin (not a pack!) to be used during a specifc state
 

src/ColibriGui/ColibriSkinManager.cpp
@@ -164,6 +164,7 @@ namespace Colibri
 				skinInfo.stateInfo.uvTopLeftBottomRight[GridLocations::Center] =
 						Ogre::Vector4( 0, 0, 1, 1 );
 				skinInfo.stateInfo.centerAspectRatio = 1.0f;
+				skinInfo.stateInfo.defaultColour = Ogre::ColourValue::White;
 
 				const rapidjson::Value &skinValue = itor->value;
 
@@ -352,6 +353,23 @@ namespace Colibri
 							skinInfo.stateInfo.borderSize[i] = borderSizeRepeat.x;
 							skinInfo.stateInfo.borderRepeatSize[i] = borderSizeRepeat.y;
 						}
+					}
+				}
+
+				itTmp = skinValue.FindMember( "colour" );
+				if( itTmp != skinValue.MemberEnd() &&  //
+					itTmp->value.IsArray() &&
+					( itTmp->value.Size() == 3u || itTmp->value.Size() == 4u ) &&
+					itTmp->value[0].IsDouble() &&  //
+					itTmp->value[1].IsDouble() &&  //
+					itTmp->value[2].IsDouble() &&
+					( itTmp->value.Size() == 3u || itTmp->value[3].IsDouble() ) )
+				{
+					const size_t numElements = itTmp->value.Size();
+					for( size_t i = 0u; i < numElements; ++i )
+					{
+						skinInfo.stateInfo.defaultColour[i] =
+							static_cast<float>( itTmp->value[i].GetDouble() );
 					}
 				}
 

src/ColibriGuiGameState.cpp
@@ -151,14 +151,13 @@ namespace Demo
 		progressBar0 = colibriManager->createWidget<Colibri::Progressbar>( mainWindow );
 		progressBar0->m_minSize = Ogre::Vector2( 350, 64 );
 		progressBar0->setProgress( 0.75f );
-		progressBar0->getProgressLayer()->m_colour = Ogre::ColourValue( 0.2f, 0.7f, 0.0f );
 		layout->addCell( progressBar0 );
 
 		progressBar1 = colibriManager->createWidget<Colibri::Progressbar>( mainWindow );
 		progressBar1->m_minSize = Ogre::Vector2( 350, 64 );
 		progressBar1->setProgress( 0.75f );
 		progressBar1->setVertical( true );
-		progressBar1->getProgressLayer()->m_colour = Ogre::ColourValue( 0.0f, 0.7f, 0.2f );
+		progressBar1->getProgressLayer()->setColour( true, Ogre::ColourValue( 0.0f, 0.7f, 0.2f ) );
 		layout->addCell( progressBar1 );
 
 		{

commit 1556721f89fb3b549c9f9061f7e4a6804c26a896

Fixed disapearing text after editing it or switching to any state (#24) Fixes #24
This is a regression introduced in 44200cbafa000a5402ce33e5b267210fefbb0818
The default colour should not be all 0s.

src/ColibriGui/ColibriRenderable.cpp
@@ -35,7 +35,9 @@ namespace Colibri
 		m_currVertexBufferOffset( 0 ),
 		m_visualsEnabled( true )
 	{
-		memset( m_stateInformation, 0, sizeof(m_stateInformation) );
+		memset( m_stateInformation, 0, sizeof( m_stateInformation ) );
+		for( size_t i = 0u; i < States::NumStates; ++i )
+			m_stateInformation[i].defaultColour = Ogre::ColourValue::White;
 	}
 	//-------------------------------------------------------------------------
 	void Renderable::_notifyCanvasChanged()