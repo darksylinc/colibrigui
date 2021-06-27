21cdf4e1d0a4f915b93093aa1ae36f212929c8bb
edherbert <edwardherbert803@gmail.com>
2021-06-27 09:42:43 +0100

    Added cmake option for flexibility. Extra checks to flexibility define.

--- include/ColibriGui/ColibriGuiPrerequisites.h
+++ include/ColibriGui/ColibriGuiPrerequisites.h
@@ -31,7 +31,7 @@
 	#define colibri_final
 #endif
 
-#if COLIBRI_FLEXIBILITY_LEVEL > 1
+#if defined( COLIBRI_FLEXIBILITY_LEVEL ) && COLIBRI_FLEXIBILITY_LEVEL > 1
 	#define colibri_virtual_l1 virtual
 #else
 	#define colibri_virtual_l1
