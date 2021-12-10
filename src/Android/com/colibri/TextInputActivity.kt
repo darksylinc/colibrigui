4b9d0afca9424e27bded1dc3808c900485a1069c
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2021-12-10 11:54:54 -0300

    Remove utils from java namespace

--- src/Android/com/colibri/TextInputActivity.kt
+++ src/Android/com/colibri/TextInputActivity.kt
@@ -17,7 +17,7 @@
 // CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
 // THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-package com.colibri.utils
+package com.colibri
 
 import android.app.Activity
 import android.app.Dialog
