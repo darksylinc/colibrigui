3f55e12b421d07008f087b4a0d400a53c55c52f0
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2022-02-18 21:04:57 -0300

    Button now supports custom-set margins to its label Particularly useful when text horiz alignment isn't center Update clang format files to clang format 13

--- src/ColibriGui/ColibriButton.cpp
+++ src/ColibriGui/ColibriButton.cpp
@@ -1,13 +1,12 @@
 
 #include "ColibriGui/ColibriButton.h"
-#include "ColibriGui/ColibriManager.h"
+
 #include "ColibriGui/ColibriLabel.h"
+#include "ColibriGui/ColibriManager.h"
 
 namespace Colibri
 {
-	Button::Button( ColibriManager *manager ) :
-		Renderable( manager ),
-		m_label( 0 )
+	Button::Button( ColibriManager *manager ) : Renderable( manager ), m_label( 0 )
 	{
 		m_clickable = true;
 		m_keyboardNavigable = true;
@@ -23,11 +22,11 @@ namespace Colibri
 	{
 		Renderable::_destroy();
 
-		//m_label is a child of us, so it will be destroyed by our super class
+		// m_label is a child of us, so it will be destroyed by our super class
 		m_label = 0;
 	}
 	//-------------------------------------------------------------------------
-	Label* Button::getLabel()
+	Label *Button::getLabel()
 	{
 		if( !m_label )
 		{
@@ -40,13 +39,19 @@ namespace Colibri
 		return m_label;
 	}
 	//-------------------------------------------------------------------------
-	void Button::sizeToFit( float maxAllowedWidth,
-							TextHorizAlignment::TextHorizAlignment newHorizPos,
-							TextVertAlignment::TextVertAlignment newVertPos,
-							States::States baseState )
+	void Button::sizeToFit( float maxAllowedWidth, TextHorizAlignment::TextHorizAlignment newHorizPos,
+							TextVertAlignment::TextVertAlignment newVertPos, States::States baseState )
 	{
 		m_label->sizeToFit( maxAllowedWidth, newHorizPos, newVertPos, baseState );
-		const Ogre::Vector2 maxSize( calculateChildrenSize() + getBorderCombined() );
+		Ogre::Vector2 maxSize( calculateChildrenSize() + getBorderCombined() );
+		if( m_label )
+		{
+			// If m_label has already been positioned, then calculateChildrenSize
+			// already accounted for one m_label->m_margin, thus we must subtract it
+			// If m_label hasn't been positioned yet, then getLocalTopLeft will be
+			// 0 and m_margin * 2.0 will be correct.
+			maxSize += m_label->m_margin * 2.0f - m_label->getLocalTopLeft();
+		}
 		setSize( maxSize );
 	}
 	//-------------------------------------------------------------------------
@@ -54,10 +59,13 @@ namespace Colibri
 	{
 		if( m_label )
 		{
-			const Ogre::Vector2 sizeAfterClipping = getSizeAfterClipping();
+			const Ogre::Vector2 sizeAfterClipping = getSizeAfterClipping() - m_label->m_margin * 2.0f;
 			if( m_label->getSize() != sizeAfterClipping )
-				m_label->setSize( sizeAfterClipping );
+			{
+				m_label->setTopLeft( m_label->m_margin );
+				m_label->setSize( sizeAfterClipping - m_label->m_margin * 2.0f );
+			}
 		}
 		Renderable::setTransformDirty( dirtyReason );
 	}
-}
+}  // namespace Colibri
